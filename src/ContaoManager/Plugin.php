524b121f90d10f4b33b5feb5bddb46f4579cf884
fbliss <fred.bliss@intelligentspark.com>
2018-05-08 17:07:41 -0400

    added plugin file, updated composer file.

---
+++ src/ContaoManager/Plugin.php
@@ -0,0 +1,29 @@
+<?php
+declare(strict_types=1);
+/*
+ * rootcontent extension for Contao Open Source CMS
+ *
+ * @copyright  Copyright (c) 2018, terminal42 gmbh
+ * @author     terminal42 gmbh <info@terminal42.ch>
+ * @license    LGPL-3.0-or-later
+ * @link       http://github.com/terminal42/contao-asset-reload
+ */
+namespace IntelligentSpark\Contao4RandomArticleBundle\ContaoManager;
+
+use Contao\CoreBundle\ContaoCoreBundle;
+use Contao\ManagerPlugin\Bundle\BundlePluginInterface;
+use Contao\ManagerPlugin\Bundle\Config\BundleConfig;
+use Contao\ManagerPlugin\Bundle\Parser\ParserInterface;
+use Terminal42\RootcontentBundle\Terminal42RootcontentBundle;
+class Plugin implements BundlePluginInterface
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function getBundles(ParserInterface $parser)
+    {
+        return [
+            BundleConfig::create(Contao4RandomArticleBundle::class)->setLoadAfter([ContaoCoreBundle::class]),
+        ];
+    }
+}
